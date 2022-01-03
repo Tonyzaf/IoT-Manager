c2f15a8ee956567484324f09b6e778feec193e07
Antonis Zafeirelis <zaftony@hotmail.gr>
2022-01-03 19:34:14 +0100

    auth0 authentication implemented

---
+++ src/components/LoginButton.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useAuth0 } from '@auth0/auth0-react';
+
+const LoginButton = () => {
+    const { loginWithRedirect, isAuthenticated } = useAuth0();
+
+    return (
+        !isAuthenticated && (
+
+            <>
+            <div class = "Welcome">
+                <h2>Καλωσήρθατε στο IoT-Manager</h2>
+            </div>
+            <div class="container">
+                    <div class="center">
+                        <button class="btn" onClick={() => loginWithRedirect()}>
+                            <svg width="180px" height="60px" viewBox="0 0 180 60" class="border">
+                                <polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
+                                <polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
+                            </svg>
+                            <span>ΣΥΝΔΕΘΕΙΤΕ ΕΔΩ</span>
+                        </button>
+                    </div>
+                </div>
+            </>
+        )
+    )
+}
+
+export default LoginButton
